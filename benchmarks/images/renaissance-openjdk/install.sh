7313ae26b1f0181e38f5a52d1ed297c2f65beaa5
Tomas Drozdik <drozdik.tomas@gmail.com>
2022-03-14 00:55:22 +0100

    Run multiple duets with YAML config
    Remove all arguments from the duetbench.py script in favor of YAML
    config that specifies all the duet benchmarks to do in single run.
    
    Add option to run duet multiple times with different ordering of A and B.

--- benchmarks/images/renaissance-openjdk/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -L -O https://github.com/renaissance-benchmarks/renaissance/releases/download/v0.13.0/renaissance-gpl-0.13.0.jar
--- benchmarks/images/renaissance-openjdk/run.sh
+++ benchmarks/images/renaissance-openjdk/run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-java -jar renaissance-gpl-0.13.0.jar --json results/results.json --repetitions 100 chi-square
+usage="./run.sh <benchmark> <repetitions>"
+
+if [ $# -ne 2 ] then
+    echo $usage
+    exit 1
+fi
+
+java -jar renaissance-gpl-0.13.0.jar --json results/results.json --repetitions $2 $1
