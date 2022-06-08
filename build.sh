a79f0d966bf670d5fc225a492a8418e0c3223654
Tomas Drozdik <drozdik.tomas@gmail.com>
2022-06-08 15:26:52 +0200

    Update SPECCpu benchmark image

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ set -e
 while getopts d: flag ; do
     case "${flag}" in
         d) docker=${OPTARG};;
+        s) speccpu_path=${OPTARG};;
+        c) speccpu_config=${OPTARG};;
     esac
 done
 
@@ -13,3 +15,8 @@ docker=${docker:-docker}
 for suite in "renaissance" "scalabench" "dacapo" ; do
     pushd ./benchmarks/${suite} && ${docker} build -t ${suite} . && popd
 done
+
+# Build speccpu suite (cca 40min)
+speccpu_path=${speccpu_path:-speccpu.zip}
+speccpu_config=${speccpu_config:-duet.yml};;
+pushd ./benchmarks/${suite} docker build --build-arg SPECCPU_ZIP=${speccpu_path} --build-arg BUILD="$(./build_args_from_duet_conf.py ${speccpu_config})" -t speccpu . && popd
