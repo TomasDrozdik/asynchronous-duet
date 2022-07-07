cba57f90ec0f561d04117257a398df5d292b37e5
Tomas Drozdik <tdrozdik@purestorage.com>
2022-07-07 15:56:07 +0200

    Package and run scripts on AWS EC2 Ubuntu instance
    First create archive using `make-archive.sh`.
    Then make this archive available on some HTTP server accessibe from EC2 instance.
    Run `run-instance.sh <path_to_archive>` on fresh EC2 Ubuntu22 instance.
    It will download, install and run all configs from the package.

---
+++ run-instance.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Initialize and run duetbench on fresh AWS Ubuntu22 AMI Instance
+#
+# from: https://github.com/TomasDrozdik/asynchronous-duet
+#
+# Prerequisites:
+# - Ubuntu22 EC2 instance
+# - Existing duetbench tar archive on remote server that is accesible for reading from the instance
+#     - see TODO(Get archive)
+#     - archive has fixed structure created by `make-archive.sh`
+# - Existing ftp server to publish results to
+#     - see TODO(Send results)
+
+set -euo pipefail
+
+HELP="run-instance.sh <path_to_duetbench_archive>"
+
+# Get duetbench
+ARCHIVE_PATH=${1:?${HELP}}
+ARCHIVE="duetbench.tar.gz"
+# TODO(Get archive) wget ${ARCHIVE_PATH} -O ${ARCHIVE}
+tar -xvf duetbench.tar.gz
+rm ${ARCHIVE}
+
+# Install packages
+sudo apt update -y
+sudo apt install -y python3 python3-pip podman
+export PATH="$HOME/.local/bin:$PATH"
+
+# Load docker images
+cd duetbench
+for image in $(find . -name "*.tar") ; do
+    podman image load --input ${image}
+done
+
+# Install duetbench
+pip install duet.tar.gz
+
+# Run duetbench
+configs=$(find . -name "*duet.yml" | tr "\n" " ")
+outdir="results.drozdikt.$(hostname -f).$(date '+%s')"
+log=${outdir}.log
+bash -c "duetbench --outdir ${outdir} --verbose --docker podman -- ${configs} &> ${log}"
+
+# TODO(Send results)
+tar=${outdir}.tar
+tar -cvzf ${tar} ${outdir} ${log}
+echo sftp anonymous@shiva.ms.mff.cuni.cz:~ <<< "put ${tar}"
+
+# End
+echo sudo poweroff
