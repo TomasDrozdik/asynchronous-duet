ff54c3ee4c64eef8880f404778a537c802a50b01
Tomas Drozdik <tdrozdik@purestorage.com>
2022-07-12 11:09:22 +0200

    run-instance: run in loop, upload partial results

--- run-instance.sh
+++ run-instance.sh
@@ -67,12 +67,14 @@ pip install duet.tar.gz
 
 # Run duetbench
 configs=$(find . -name "*duet.yml" | tr "\n" " ")
-outdir="results.drozdikt.$(hostname -f).$(date '+%Y-%m-%d--%H-%M-%S--%s')"
-log=${outdir}.log
-bash -c "duetbench --outdir ${outdir} --verbose --docker podman -- ${configs} &> ${log}"
+for i in $(seq 4) ; do
+    outdir="results.drozdikt.$(hostname -f).${i}.$(date '+%Y-%m-%d--%H-%M-%S--%s')"
+    log=${outdir}.log
+    bash -c "duetbench --outdir ${outdir} --verbose --docker podman -- ${configs} &> ${log}"
 
-tarball="${outdir}.tar.gz"
-tar -cvzf "${tarball}" "${outdir}" "${log}"
-curl --upload-file "${tarball}" "${FTP_PATH}/${tarball}"
+    tarball="${outdir}.tar.gz"
+    tar -cvzf "${tarball}" "${outdir}" "${log}"
+    curl --upload-file "${tarball}" "${FTP_PATH}/${tarball}"
+done
 
 sudo poweroff
